31a5b4e6911cbc4016a976f951a4f8d62cec5597
Auston Stewart <auston@shedbuilt.net>
2020-10-14 10:25:56 -1000

    Updating with X11 and EGL options

--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_X11_OPTION='1'
+SHED_PKG_LOCAL_EGL_OPTION='1'
+for SHED_PKG_LOCAL_OPTION in "${!SHED_PKG_LOCAL_OPTIONS[@]}"; do
+    case "$SHED_PKG_LOCAL_OPTION" in
+        x11)
+            SHED_PKG_LOCAL_X11_OPTION='0'
+            ;;
+        egl)
+            SHED_PKG_LOCAL_EGL_OPTION='0'
+            ;;
+    esac
+done
 # Build and Install
-mkdir -pv build
-cd build
-cmake -DODROID=1                       \
-      -DNOX11=1                        \
-      -DDEFAULT_ES=2                   \
-      -DCMAKE_INSTALL_PREFIX=/usr      \
-      .. &&                            \
-make -j $SHED_NUM_JOBS &&              \
-make DESTDIR="$SHED_FAKE_ROOT" install
+cmake -DODROID=1                         \
+      -DNOEGL=$SHED_PKG_LOCAL_EGL_OPTION \
+      -DNOX11=$SHED_PKG_LOCAL_X11_OPTION  \
+      -DDEFAULT_ES=2                     \
+      -DCMAKE_INSTALL_PREFIX=/usr        \
+      .. &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Rearrange and install symlinks
+mv "${SHED_FAKE_ROOT}/usr/lib/gl4es/libGL.so.1" "${SHED_FAKE_ROOT}/usr/lib/" &&
+rmdir "${SHED_FAKE_ROOT}/usr/lib/gl4es" &&
+ln -sv libGL.so.1 "${SHED_FAKE_ROOT}/usr/lib/libGL.so"
